da093019f75b47f35424a1c964a03bd12a191c9f
Mike Freeman <michael.k.freeman@gmail.com>
2020-02-10 12:29:51 -0800

    Update to interactive page

--- ajax/activity-2/js/index.js
+++ ajax/activity-2/js/index.js
@@ -1,16 +1,58 @@
 'use strict';
+let state = {};
 
-// Use d3 to load the csv file using a *relative path*
-d3.csv("data/websites.csv").then(function(data) {
-    console.log(data)
-    // Iterate through the data and append a new <li> for each item
-    // The <li> should contain a link (<a>) to the website and 
-    // a <span> with the description about the website
-    data.map(function(d){
-        let li = $('<li>');
-        let a = $(`<a href="${d.url}">${d.title}</a>`);
-        li.append(a);
-        li.append(`<span>: ${d.description}</span>`)
-        $('ul').append(li);
-    })
+// Function to render an item
+function renderItem(item, parent) {
+    let li = $('<li>');
+    let a = $(`<a href="${item.url}">${item.title}</a>`);
+    li.append(a);
+    li.append(`<span>: ${item.description}</span>`)
+    parent.append(li);
+}
+
+// Load data
+d3.csv("data/websites.csv").then(function(data) {    
+    state.data = data;    
+    updatePage();
 })
+
+// Function to render list of items
+function renderList() {
+    $("ul").empty();
+    state.data.map(function(d){
+        renderItem(d, $('ul'))
+    })
+}
+
+// Track input state
+$("input").on("input", function() {
+    let id = $(this).attr("id");
+    state[id] = $(this).val();
+    updateButton();
+})
+
+// Event (push in data, update page)
+$("form").on("submit", function() {
+    event.preventDefault();
+    state.data.push({url:state.url, description:state.description, title:state.title});
+    updatePage();    
+})
+
+
+// Update page
+function updatePage() {
+    // Clear values
+    $("input").val("");
+    state.url = '';
+    state.description = '';
+    state.title = '';
+    $("#page_title").text("Top " + state.data.length + " Blogs")
+    renderList();
+    updateButton();
+}
+
+// Toggle button class
+function updateButton() {
+    let klass = state.input == "" | state.url == "" | state.title == "" ? true : false; 
+    $("button").attr("disabled", klass);
+}
